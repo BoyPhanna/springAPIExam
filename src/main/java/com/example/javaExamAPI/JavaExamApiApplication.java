2ea8af0aa48b6390a2a917b5693d917c1cd03da7
BoyPhanna <kimphanna56@gmail.com>
2024-07-18 19:55:09 +0700

    1

---
+++ src/main/java/com/example/javaExamAPI/JavaExamApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaExamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaExamAPI/JavaExamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaExamAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
